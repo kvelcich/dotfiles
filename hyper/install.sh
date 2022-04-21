a9a01de8cdffebd0084fb7737f26c6112929ed3f
Kevin Velcich <kvelcich@gmail.com>
2022-04-21 16:06:40 -0700

    Update install.sh

--- hyper/install.sh
+++ hyper/install.sh
@@ -3,15 +3,16 @@
 # Determine directory to place resources in
 HYPERDIR=$HOME
 if grep -q wsl <<< $1; then
-    # TODO: automatically figure out user name here
-    HYPERDIR=/mnt/c/Users/kvel/AppData/Roaming/Hyper
+    # TODO: assumes user is installed to C drive
+    WIN_USER=$(powershell.exe '$env:UserName'
+    HYPERDIR="/mnt/c/${WIN_USER::-1}/AppData/Roaming/Hyper"
 fi
 
 # Download hyper-kevin colorscheme (currently just as a local plugin)
 mkdir -p $HYPERDIR/.hyper_plugins/local
 rm -rf $HYPERDIR/.hyper_plugins/local/hyper-kev
 git clone https://github.com/kvelcich/hyper-kev.git \
-    $HYPERDIR/.hyper_plugins/local/hyper-kev
+    $HYPERDIR/.hyper_plugins/local/
 
 # Copying hyper resources
 cp $(dirname $0)/.hyper.js $HYPERDIR/.
