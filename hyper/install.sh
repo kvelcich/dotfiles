ec431cba5c89de13fbcf144debd22076f910b313
Kevin Velcich <kvelcich@gmail.com>
2022-04-21 16:27:33 -0700

    Update install.sh

--- hyper/install.sh
+++ hyper/install.sh
@@ -2,13 +2,12 @@
 
 # Determine directory to place resources in
 HYPERDIR=$HOME
-if grep -q wsl <<< $1; then
-    # TODO: assumes user is installed to C drive
+if [[ "$OSTYPE" == "linux-gnu" ]] && grep -qi microsoft /proc/version; then     
     WIN_USER=$(powershell.exe '$env:UserName')
     HYPERDIR="/mnt/c/${WIN_USER::-1}/AppData/Roaming/Hyper"
 fi
 
-# Download hyper-kevin colorscheme (currently just as a local plugin)
+# Download hyper-kev colorscheme (currently just as a local plugin)
 mkdir -p $HYPERDIR/.hyper_plugins/local
 rm -rf $HYPERDIR/.hyper_plugins/local/hyper-kev
 git clone https://github.com/kvelcich/hyper-kev.git \
@@ -17,5 +16,4 @@ git clone https://github.com/kvelcich/hyper-kev.git \
 # Copying hyper resources
 cp $(dirname $0)/.hyper.js $HYPERDIR/.
 
-# TODO: Do something about installing the Roboto Mono Font to use
-
+# TODO: Do something about installing the fonts
