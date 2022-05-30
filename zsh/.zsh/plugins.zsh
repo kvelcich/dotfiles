d87131f83a593c8c1252ac5a35efcf0fbee8a936
Kevin Velcich <kvelcich@gmail.com>
2022-05-29 18:23:01 -0700

    Update plugins.zsh

--- zsh/.zsh/plugins.zsh
+++ zsh/.zsh/plugins.zsh
@@ -1,6 +1,5 @@
 source "$HOME/.zsh/antigen/antigen.zsh"
 
-antigen bundle command-not-found
 antigen bundle mafredri/zsh-async
 antigen bundle sindresorhus/pure
 antigen bundle zsh-users/zsh-syntax-highlighting
